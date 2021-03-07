33f813cd35413afd399740462ad69ad5fb459fc2
Dylan Feres <dferes23@gmail.com>
2021-03-06 19:00:55 -0800

    added some documentation

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,16 +1,25 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/**
+ * Takes two parameters: 
+ *    dataToJoin: an object containing field names to be changed, such as username
+ *    jsToSql:    an object with key names in camelCase and values with the same name,
+ *                but in snake_case (eg. { firstName: 'first_name'} )
+ * 
+ * Returns an object containing:
+ *    a string in the form of "variable_name" =$1 to be used as input for a SQL query
+ *    a list of the updated field values  
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
-
+  
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
-
+  
   return {
     setCols: cols.join(", "),
     values: Object.values(dataToUpdate),
