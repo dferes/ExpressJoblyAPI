8410e3df523e70e5227f1fc564df25eabc5feb60
Dylan Feres <dferes23@gmail.com>
2021-03-12 09:31:17 -0800

    added first route for POST: /

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,12 +7,33 @@ const express = require("express");
 
 const { BadRequestError } = require("../expressError");
 const { ensureLoggedIn, verifyIsAdmin } = require("../middleware/auth");
-const Company = require("../models/company");
+const Job = require("../models/job");
 
-// need to import json schemas here 
+const jobNewSchema = require("../schemas/jobNew.json");
 
 const router = new express.Router();
 
-
+/** POST / { job } =>  { job }
+ *
+ * job should be { title, salary, equity, companyHandle }
+ *
+ * Returns { id, title, salary, equity, companyHandle }
+ *
+ * Authorization required: Admin
+ */
+ router.post("/", [ensureLoggedIn, verifyIsAdmin], async function (req, res, next) {
+    try {
+      const validator = jsonschema.validate(req.body, jobNewSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+      }
+  
+      const job = await Job.create(req.body);
+      return res.status(201).json({ job });
+    } catch (err) {
+      return next(err);
+    }
+  });
 
 module.exports = router
