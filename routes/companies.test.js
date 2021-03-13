5dc00d461c3a1659954b43b4fac942abfeb5e79c
Dylan Feres <dferes23@gmail.com>
2021-03-12 22:51:01 -0800

    modified and added tests to accomodate the inclusion of returning all jobs associated with a company handle

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -197,29 +197,50 @@ describe("GET /companies/:handle", function () {
   test("works for anon", async function () {
     const resp = await request(app).get(`/companies/c1`);
     expect(resp.body).toEqual({
-      company: {
-        handle: "c1",
-        name: "C1",
-        description: "Desc1",
-        numEmployees: 1,
-        logoUrl: "http://c1.img",
-      },
+      handle: "c1",
+      name: "C1",
+      description: "Desc1",
+      numEmployees: 1,
+      logoUrl: "http://c1.img",
+      jobs: []
     });
   });
 
   test("works for anon: company w/o jobs", async function () {
     const resp = await request(app).get(`/companies/c2`);
     expect(resp.body).toEqual({
-      company: {
-        handle: "c2",
-        name: "C2",
-        description: "Desc2",
-        numEmployees: 2,
-        logoUrl: "http://c2.img",
-      },
+      handle: "c2",
+      name: "C2",
+      description: "Desc2",
+      numEmployees: 2,
+      logoUrl: "http://c2.img",
+      jobs: []
     });
   });
-
+  test("works for anon: company with jobs", async function () {
+    const resp = await request(app).get(`/companies/c3`);
+    console.log('++++++++++++>>>>>>>>', resp.body);
+    expect(resp.body).toEqual({
+      handle: "c3",
+      name: "C3",
+      description: "Desc3",
+      numEmployees: 3,
+      logoUrl: "http://c3.img",
+      jobs: [{
+        id: expect.any(Number),
+        title: 'Accountant',
+        salary: 89000,
+        equity: '0.25'
+      },
+      {
+        id: expect.any(Number),
+        title: 'Manager',
+        salary: 103000,
+        equity: '0.125'
+      }
+    ]
+    });
+  });
   test("not found for no such company", async function () {
     const resp = await request(app).get(`/companies/nope`);
     expect(resp.statusCode).toEqual(404);
