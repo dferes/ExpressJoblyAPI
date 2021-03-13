fc6bfc3b4739c18eee53058dc0012ba7a972630f
Dylan Feres <dferes23@gmail.com>
2021-03-12 16:42:28 -0800

    added 2 of 5 routes

--- routes/jobs.js
+++ routes/jobs.js
@@ -10,6 +10,7 @@ const { ensureLoggedIn, verifyIsAdmin } = require("../middleware/auth");
 const Job = require("../models/job");
 
 const jobNewSchema = require("../schemas/jobNew.json");
+const jobSearchFilter = require("../schemas/jobSearchFilter.json");
 
 const router = new express.Router();
 
@@ -35,5 +36,35 @@ const router = new express.Router();
       return next(err);
     }
   });
+
+/** GET /  =>
+ *   { jobs: [ { id, title, salary, equity, companyHandle }, ...] }
+ *
+ * Can filter on provided search filters:
+ * 
+ * - title (will find case-insensitive, partial matches)
+ * - minSalary
+ * - maxSalary
+ *
+ * Authorization required: none
+ */
+ router.get("/", async (req, res, next) => {
+  try {
+    const validator = jsonschema.validate(req.body, jobSearchFilter);
+    // console.log('-------------->>>>', validator.valid);
+    if (!validator.valid) {
+      const errs = validator.errors.map(e => e.stack);
+      throw new BadRequestError(errs);
+    }
+    // console.log('===========>', req.body);  
+    const jobs = await Job.findAll(req.body);
+    return res.json({ jobs });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+
+
 
 module.exports = router
