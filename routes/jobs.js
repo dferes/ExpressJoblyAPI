abbcfb637c1fb72d63bbf28e0ce479fb76aef9b9
Dylan Feres <dferes23@gmail.com>
2021-03-15 21:39:20 -0700

    added json schema for adding job applications to the applications table

--- routes/jobs.js
+++ routes/jobs.js
@@ -6,12 +6,13 @@ const jsonschema = require("jsonschema");
 const express = require("express");
 
 const { BadRequestError } = require("../expressError");
-const { ensureLoggedIn, verifyIsAdmin } = require("../middleware/auth");
+const { ensureLoggedIn, verifyIsAdmin, ensureAdminOrOwner } = require("../middleware/auth");
 const Job = require("../models/job");
 
 const jobNewSchema = require("../schemas/jobNew.json");
 const jobSearchFilter = require("../schemas/jobSearchFilter.json");
 const jobUpdateSchema = require("../schemas/jobUpdateSchema.json");
+const jobApplicationSchema = require("../schemas/jobApplicationSchema.json");
 
 const router = new express.Router();
 
